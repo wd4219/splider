fdbb3bd51a57771d815957c2d028be1b3d8e8ee7
wd4219 <57272276@qq.com>
2017-04-13 20:30:36 +0800

    优化爬虫

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 const http = require('http');
 const DB_URL = 'mongodb://localhost/xiaodiwaimai';
+const superagent = require('superagent');
+const OSS = require('ali-oss').Wrapper;
 
-mongoose.connect(DB_URL);
+// mongoose.connect(DB_URL);
 // mongoose.set('debug', true);
 
 // tools.empty();
@@ -22,4 +24,21 @@ mongoose.connect(DB_URL);
 //     },i*2000);
 //   }
 // });
-Image.find(file.downLoadImage);
+// Image.find(file.downLoadImage);
+var req = superagent.get('http://fuss10.elemecdn.com/a/79/0b3414e103133ab4134ef75593d53jpeg.jpeg?imageMogr/format/webp/').set({
+  Origin: 'https://h5.ele.me',
+  Referer: 'https://h5.ele.me/msite/',
+});
+var stream = fs.createReadStream('./download/dajuldajkl.jpeg');
+var client = new OSS({
+  region: 'oss-cn-shenzhen',
+  accessKeyId: 'LTAIQM3IZQg4blnJ',
+  accessKeySecret: 'G5Wni5yILnzAITPceB56BVZcmrQ3mP',
+  bucket:'xiaodiwaimai'
+});
+
+client.putStream('icon.jpeg', stream).then(function(res){
+  console.log(res);
+}).catch(function(err){
+  console.log(err);
+});
--- file/index.js
+++ file/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 
+
 exports.downLoadImage = function (result) {
   // result.forEach(function (item) {
   //   http.get(item.url, function (res) {
@@ -20,20 +21,15 @@ exports.downLoadImage = function (result) {
   //     });
   //   });
   // });
-   http.get('http://fuss10.elemecdn.com/a/79/0b3414e103133ab4134ef75593d53jpeg.jpeg?imageMogr/format/webp/', function (res) {
-      var imgData = "";
-      res.setEncoding("binary");
-      res.on("data", function (chunk) {
-        imgData += chunk;
-      });
-      res.on("end", function () {
-        var path = './download/' + 'dajuldajkl.jpeg';
-        fs.writeFile(path, imgData, "binary", function (err) {
-          if (err) {
-            console.log("down fail");
-          }
-          console.log("down success");
-        });
-      });
+  superagent.get('http://fuss10.elemecdn.com/a/79/0b3414e103133ab4134ef75593d53jpeg.jpeg?imageMogr/format/webp/', function (res) {
+    console.log(res);
+    var path = './download/' + 'dajuldajkl.jpeg';
+    fs.writeFile(path, imgData, "binary", function (err) {
+      if (err) {
+        console.log("down fail");
+      }
+      console.log("down success");
+
     });
+  });
 }
